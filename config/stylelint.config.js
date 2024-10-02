8be3559f8a2853d94166eddcf5d0e55fb098546c
inoue <emocedigital@gmail.com>
2024-10-02 20:48:01 +0900

    configの設定変更

--- config/stylelint.config.js
+++ config/stylelint.config.js
@@ -14,5 +14,6 @@ module.exports = {
     'selector-max-id': 0,
     'media-feature-name-no-unknown': true,
     'selector-class-pattern': null,
+    'media-feature-range-notation': 'prefix',
   },
 };
